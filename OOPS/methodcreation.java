26840f1199847c5cb77675660c7fda83a6716030
Preronakundu <116329904+Preronakundu@users.noreply.github.com>
2024-09-18 09:16:41 +0530

    Create methodcreation.java

---
+++ OOPS/methodcreation.java
@@ -0,0 +1,13 @@
+// code for creating method class that display hello while execution
+
+class Main
+{
+    void display(){
+        System.out.println("hello");
+    }
+	public static void main(String[] args) {
+		Main m2 = new Main();
+		m2.display();
+		System.out.println("end running?");
+	}
+}
